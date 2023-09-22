396981e7f67a79eab75c7fbf5d098c6aba1b8b15
codeRisshi25 <74894700+codeRisshi25@users.noreply.github.com>
2023-09-23 00:17:31 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+#This file will contain the code for the front-end of the project 
+#Import Tkinter here
 
+import tkinter
+top = tkinter.Tk()
+
+# Code to add widgets will go here...
+top.mainloop()
